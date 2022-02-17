b49be5c677589c7908ed769d3d6d2f6fb0204fa9
Victor Mours <victor.mours@gmail.com>
2022-02-17 19:10:58 +0100

    feature flag is not needed

--- config/initializers/rdv_collectif.rb
+++
@@ -1,7 +0,0 @@
-class RdvCollectif
-  AUTHORIZED_ORGANISATION_IDS = ENV["RDVC_ORGANISATION_IDS"]&.split(",")&.map(&:to_i) || []
-
-  def self.enabled_for?(organisation)
-    organisation.id.in?(AUTHORIZED_ORGANISATION_IDS)
-  end
-end
