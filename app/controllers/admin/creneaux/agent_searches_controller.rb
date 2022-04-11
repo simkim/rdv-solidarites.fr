94af971e5f959f6eaeecd0c5b8430daa5420e395
Victor Mours <victor.mours@gmail.com>
2022-04-11 12:55:10 +0100

    try to have 3 different states for rdv search

--- app/controllers/admin/creneaux/agent_searches_controller.rb
+++ app/controllers/admin/creneaux/agent_searches_controller.rb
@@ -6,25 +6,20 @@ class Admin::Creneaux::AgentSearchesController < AgentAuthController
   def index
     @form = helpers.build_agent_creneaux_search_form(current_organisation, params)
     @search_results = search_results
-
-    if @search_results&.count == 1 && @form.motif.individuel?
-      skip_policy_scope # TODO: improve pundit checks for creneaux
-
-      redirect_to admin_organisation_slots_path(current_organisation,
-                                                helpers.creneaux_search_params(@form).merge(lieu_ids: [@search_results.first.lieu.id])),
-                  class: "d-block stretched-link"
-    else
-      @motifs = policy_scope(Motif).active.ordered_by_name
-      @services = policy_scope(Service)
-        .where(id: @motifs.pluck(:service_id).uniq)
-        .ordered_by_name
-      @form.service_id = @services.first.id if @services.count == 1
-      @teams = current_organisation.territory.teams
-      @agents = policy_scope(Agent)
-        .joins(:organisations).where(organisations: { id: current_organisation.id })
-        .complete.active.order_by_last_name
-      @lieux = policy_scope(Lieu).enabled.ordered_by_name
+    if @search_results&.count == 1
+      @search_result = @search_results.first
     end
+
+    @motifs = policy_scope(Motif).active.ordered_by_name
+    @services = policy_scope(Service)
+      .where(id: @motifs.pluck(:service_id).uniq)
+      .ordered_by_name
+    @form.service_id = @services.first.id if @services.count == 1
+    @teams = current_organisation.territory.teams
+    @agents = policy_scope(Agent)
+      .joins(:organisations).where(organisations: { id: current_organisation.id })
+      .complete.active.order_by_last_name
+    @lieux = policy_scope(Lieu).enabled.ordered_by_name
   end
 
   private
