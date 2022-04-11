5040b332e9804c4fb3f28c4dab186b1a67c95a65
Victor Mours <victor.mours@gmail.com>
2022-04-11 16:43:17 +0100

    Add spec

--- spec/features/agents/finding_creneau_for_rdv_collectif_spec.rb
+++ spec/features/agents/finding_creneau_for_rdv_collectif_spec.rb
@@ -9,13 +9,12 @@ describe "Agent can find a creneau for a rdv collectif", js: true do
   let!(:service) { create(:service) }
   let!(:lieu) { create(:lieu, organisation: organisation) }
   let!(:rdv) do
-    create(:rdv, motif: motif, organisation: organisation, agents: [agent], max_participants_count: 5)
+    create(:rdv, motif: motif, organisation: organisation, agents: [agent], max_participants_count: 5, lieu: lieu)
   end
 
-  specify do
-    login_as(agent, scope: :agent)
+  before { login_as(agent, scope: :agent) }
 
-    # Creating a new RDV Collectif
+  specify do
     visit admin_organisation_agent_agenda_path(organisation, agent)
     click_link "Trouver un RDV"
 
@@ -23,12 +22,31 @@ describe "Agent can find a creneau for a rdv collectif", js: true do
     click_button "Afficher les créneaux"
 
     # The rdv collectif appears in the search results
-    expect(page).to have_content("Résultats de votre recherche")
+    expect(page).to have_content("Créneaux disponibles pour atelier participatif")
     expect(page).to have_content("1 participant")
     expect(page).to have_content("4 places disponibles")
 
     click_link "Ajouter un participant"
 
     expect(page).to have_current_path(edit_admin_organisation_rdvs_collectif_path(rdv.organisation, rdv))
+  end
+
+  context "when there are rdvs available in two different lieux" do
+    let!(:lieu2) { create(:lieu, organisation: organisation) }
+    let!(:rdv2) do
+      create(:rdv, motif: motif, organisation: organisation, agents: [agent], max_participants_count: 5, lieu: lieu2)
+    end
+
+    it "shows the list of lieux before the list of rdvs" do
+      visit admin_organisation_agent_searches_path(organisation)
+
+      select "Atelier participatif", from: "Motif"
+      click_button "Afficher les créneaux"
+
+      expect(page).to have_content("2 lieux proposent des disponibilités")
+      click_link("Prochaine disponibilité", match: :first)
+
+      expect(page).to have_content("Créneaux disponibles pour atelier participatif")
+    end
   end
 end
